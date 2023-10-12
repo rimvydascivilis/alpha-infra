5467555ce6dbab95dfc52efe82db14321617cc97
Rimvydas Civilis <rimvydascivilis@proton.me>
2023-10-12 20:02:01 +0300

    Add version requirements

--- modules/k8s-addons/0-versions.tf
+++ modules/k8s-addons/0-versions.tf
@@ -6,5 +6,15 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.11.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.23.0"
+    }
   }
 }
